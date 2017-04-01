2899eb689d57a37a2689ce978200c3c7b8e0e2cf
Jordi Martínez Frias <jordimart83@gmail.com>
2017-04-01 03:27:24 +0200

    [REM] estructura: elimino ficheros para reinstalar.

--- server/daw/app/api/v1/viewsets.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from app.games.models import Game
-from app.api.v1.serializers import GameSerializer
-
-
-class GameViewSet(viewsets.ModelViewSet):
-    queryset = Game.objects.all()
-    serializer_class = GameSerializer
